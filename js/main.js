ac736c0dbe8cdb4a52126e6488d930e17696ac56
distributorgame <raincitygamingid@gmail.com>
2025-07-29 01:17:53 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('24/7').split('')
+    const titles = ('24 7').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
