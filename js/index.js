bd60891db5382803d8d7654cd7c674e17af228d9
distributorgame <raincitygamingid@gmail.com>
2025-07-29 00:54:18 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Aku punya sesuatu`.split('')
+const text = `I Have Something`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Everyone has someone special in their life, for me it's you, anjayy🗿').split('')
+    const titles = ('I LOVE U').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
