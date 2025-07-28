4f42461a8064a95748f5ee6797e18d453486fe78
distributorgame <raincitygamingid@gmail.com>
2025-07-29 01:05:01 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('ANANDA').split('')
+    const titles = ('Everyone has someone special in their life, for me it's you').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
